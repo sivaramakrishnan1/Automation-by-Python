f760273170f2b2bf8ef3a27469252d4d6762c66b
sivaramakrishnan1 <sivageeven@gmail.com>
2020-09-29 23:24:25 +0530

    commit

--- Whatsapp_sender.py
+++ Whatsapp_sender.py
@@ -5,7 +5,7 @@ You can change the message by changing msg variable
 import pyautogui as p, time as t
 
 msg= "Your message goes here" #The message
-open_time = 30  #The time that whatsapp might take to open in slower systems
+open_time = 10  #The time that whatsapp might take to open in slower systems
 
 #opening whatsapp
 p.press('win')
@@ -20,6 +20,7 @@ t.sleep(3)
 p.press('tab')
 p.press('down')
 p.press('enter')
+t.sleep(3)
 p.write(msg) #Typing the message 
 p.press('enter')
 
