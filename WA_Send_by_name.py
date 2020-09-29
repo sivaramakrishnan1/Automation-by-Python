34a5b1c41915b10c0ff42efa3877074311ee8928
sivaramakrishnan1 <sivageeven@gmail.com>
2020-09-29 23:45:10 +0530

    commit on 'master'

---
+++ WA_Send_by_name.py
@@ -0,0 +1,25 @@
+"""
+This program will send a message to the most recent whatsapp message sent/ recieved chat. 
+You can change the message by changing msg variable
+"""
+import pyautogui as p, time as t
+
+
+contact=list(map(str, input("Enter a contact name: ").split()))
+msg= "Your message goes here" #The message
+open_time = 10  #The time that whatsapp might take to open in slower systems
+
+#opening whatsapp
+p.press('win')
+p.write('whatsapp')
+p.press('enter')
+
+#Waiting to open whatsapp
+t.sleep(open_time)
+t.sleep(3)
+
+#Navigating to the recent chat
+p.press('tab')
+
+t.sleep(3)
+p.write(msg) #Typing the message 
